66c97a1e749eef9ec1d8920a9539f0188ff1fa1a
ghvziab <s.ghaziabbas04@gmail.com>
2025-06-05 23:40:07 +0500

    chrome

---
+++ install_chrome.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y wget unzip
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb || true
+apt --fix-broken install -y
