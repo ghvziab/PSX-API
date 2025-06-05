e95bf2dee71b1a1f952059e5d59bc2a493e99433
ghvziab <s.ghaziabbas04@gmail.com>
2025-06-05 20:12:27 +0500

    flask setup

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from scraper import scheduler
+from flask import Flask, jsonify
+from scraper import scraper
+app = Flask(__name__)
+
+@app.route("/psx-market-watch")
+def psxdata():
+    data = scraper()
+    return jsonify(data)
 
 if __name__ == "__main__":
-    scheduler()
-
+    app.run(debug = True)
