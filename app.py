b51a91d273eb1a22c8fa366d4a823e34f3ed8418
ghvziab <s.ghaziabbas04@gmail.com>
2025-06-05 22:24:43 +0500

    debug fix

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
 from scraper import scraper
+import traceback
+
 app = Flask(__name__)
 
 @app.route("/psx-market-watch")
 def psxdata():
-    data = scraper()
-    return jsonify(data)
+    try:
+        data = scraper()
+        return jsonify(data)
+    except:
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug = True)
